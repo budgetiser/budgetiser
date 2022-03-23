2eab45106c65d600dfe0c4f6128502ff8743b0b6
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-03-23 16:19:17 +0100

    changes

--- budgetiser/lib/screens/newAccount.dart
+++ budgetiser/lib/screens/newAccount.dart
@@ -1,3 +1,4 @@
+import 'package:budgetiser/shared/services/selectCategory.dart';
 import 'package:flutter/material.dart';
 
 class NewAccount extends StatelessWidget {
@@ -7,12 +8,49 @@ class NewAccount extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Accounts"),
+        title: const Text("New Account"),
       ),
       body: Container(
-        child: Text('SCREEN 2'),
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            const Text("Name"),
+            MyCustomForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+          color: const Color.fromARGB(255, 255, 165, 165),
+        ),
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SelectCategory(),
+            ),
+            Flexible(
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter name',
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
