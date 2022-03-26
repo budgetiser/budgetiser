75ad6261cb64b9305f7bdb993e989edd4c3cfcef
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-03-26 21:19:55 +0100

    .

--- budgetiser/lib/screens/help.dart
+++ budgetiser/lib/screens/help.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import '../shared/widgets/drawer.dart';
+
+const String _markdownData = """
+# Minimal Markdown Test
+---
+This is a simple Markdown test. Provide a text string with Markdown tags
+to the Markdown widget and it will display the formatted output in a
+scrollable widget.
+
+## Section 1
+Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
+aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
+eget est condimentum, vitae porttitor diam ornare.
+
+### Subsection A
+Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
+venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
+accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
+ornare, in ullamcorper magna congue.
+
+## Section 1
+Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
+aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
+eget est condimentum, vitae porttitor diam ornare.
+
+### Subsection A
+Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
+venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
+accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
+ornare, in ullamcorper magna congue.
+""";
 
 class Help extends StatelessWidget {
   static String routeID = 'help';
@@ -16,7 +47,7 @@ class Help extends StatelessWidget {
       ),
       drawer: createDrawer(context),
       body: const Center(
-        child: Text("Help"),
+        child: Markdown(data: _markdownData),
       ),
     );
   }
