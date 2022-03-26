16b921ed81368e78a5661d00e3869504d514505d
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-03-26 21:38:22 +0100

    use md file

---
+++ budgetiser/assets/how-to.md
@@ -0,0 +1,33 @@
+# Minimal Markdown Test
+
+---
+
+This is a simple Markdown test. Provide a text string with Markdown tags
+to the Markdown widget and it will display the formatted output in a
+scrollable widget.
+
+## Section 1
+
+Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
+aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
+eget est condimentum, vitae porttitor diam ornare.
+
+### Subsection A
+
+Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
+venenatis libero. ****Curabitur sem lectus, feugiat eu justo in, eleifend
+accumsan ante.**** Sed a fermentum elit. Curabitur sodales metus id mi
+ornare, in ullamcorper magna congue.
+
+## Section 1
+
+Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
+aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
+eget est condimentum, vitae porttitor diam ornare.
+
+### Subsection A
+
+Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
+venenatis libero. ****Curabitur sem lectus, feugiat eu justo in, eleifend
+accumsan ante.**** Sed a fermentum elit. Curabitur sodales metus id mi
+ornare, in ullamcorper magna congue.
--- budgetiser/lib/screens/help.dart
+++ budgetiser/lib/screens/help.dart
@@ -1,36 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import '../shared/widgets/drawer.dart';
-
-const String _markdownData = """
-# Minimal Markdown Test
----
-This is a simple Markdown test. Provide a text string with Markdown tags
-to the Markdown widget and it will display the formatted output in a
-scrollable widget.
-
-## Section 1
-Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
-aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
-eget est condimentum, vitae porttitor diam ornare.
-
-### Subsection A
-Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
-venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
-accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
-ornare, in ullamcorper magna congue.
-
-## Section 1
-Maecenas eget **arcu egestas**, mollis ex vitae, posuere magna. Nunc eget
-aliquam tortor. Vestibulum porta sodales efficitur. Mauris interdum turpis
-eget est condimentum, vitae porttitor diam ornare.
-
-### Subsection A
-Sed et massa finibus, blandit massa vel, vulputate velit. Vestibulum vitae
-venenatis libero. **__Curabitur sem lectus, feugiat eu justo in, eleifend
-accumsan ante.__** Sed a fermentum elit. Curabitur sodales metus id mi
-ornare, in ullamcorper magna congue.
-""";
 
 class Help extends StatelessWidget {
   static String routeID = 'help';
@@ -42,13 +13,27 @@ class Help extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           "Help",
-          // style: Theme.of(context).textTheme.caption,
         ),
       ),
       drawer: createDrawer(context),
-      body: const Center(
-        child: Markdown(data: _markdownData),
+      body: FutureBuilder(
+        future: rootBundle.loadString("assets/how-to.md"),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Markdown(
+              data: snapshot.data!,
+              // styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
+      // const Center(
+      //   child: Markdown(data: _markdownData),
+      // ),
     );
   }
 }
