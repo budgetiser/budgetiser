f80578775c53698196fee82486ee2b822ec845f6
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-04-02 13:43:31 +0200

    sorting

--- budgetiser/lib/screens/account/accountScreen.dart
+++ budgetiser/lib/screens/account/accountScreen.dart
@@ -1,17 +1,65 @@
 import 'package:budgetiser/screens/account/newAccount.dart';
 import 'package:budgetiser/shared/tempData/tempData.dart';
 import 'package:flutter/material.dart';
+import '../../shared/dataClasses/account.dart';
 import '../../shared/services/accountItem/accountItem.dart';
 import '../../shared/widgets/drawer.dart';
 
-class AccountScreen extends StatelessWidget {
+class AccountScreen extends StatefulWidget {
   static String routeID = 'account';
-  const AccountScreen({Key? key}) : super(key: key);
+  AccountScreen({
+    Key? key,
+  }) : super(key: key);
 
+  List<Account> accountList = TMP_DATA_accountList;
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.sort),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return SimpleDialog(
+                    title: const Text('Sort by'),
+                    alignment: Alignment.topRight,
+                    children: <Widget>[
+                      SimpleDialogOption(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            widget.accountList
+                                .sort((a, b) => a.name.compareTo(b.name));
+                          });
+                        },
+                        child: const Text('Name'),
+                      ),
+                      SimpleDialogOption(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          setState(() {
+                            widget.accountList
+                                .sort((a, b) => a.balance.compareTo(b.balance));
+                          });
+                        },
+                        child: const Text('Balance'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
         title: const Text(
           "Accounts",
         ),
@@ -21,10 +69,13 @@ class AccountScreen extends StatelessWidget {
         child: Center(
           child: Column(
             children: [
-              for (var account in TMP_DATA_accountList)
+              for (var account in widget.accountList)
                 AccountItem(
                   accountData: account,
                 ),
+              const SizedBox(
+                height: 80,
+              ),
             ],
           ),
         ),
