c38faca42398f5d062396253d010c8cdc0fac16e
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-04-30 22:16:15 +0200

    fix transactionItem

--- budgetiser/lib/screens/transactions/transactionsScreen.dart
+++ budgetiser/lib/screens/transactions/transactionsScreen.dart
@@ -1,6 +1,7 @@
 import 'package:budgetiser/db/database.dart';
 import 'package:budgetiser/screens/transactions/transactionForm.dart';
 import 'package:budgetiser/shared/dataClasses/account.dart';
+import 'package:budgetiser/shared/dataClasses/recurringTransaction.dart';
 import 'package:budgetiser/shared/dataClasses/singleTransaction.dart';
 import 'package:budgetiser/shared/widgets/transactionItem.dart';
 import 'package:budgetiser/drawer.dart';
@@ -19,10 +20,19 @@ class TransactionsScreen extends StatefulWidget {
   State<TransactionsScreen> createState() => _TransactionsScreenState();
 }
 
+enum pages { singleTransactions, recurringTransactions }
+
 class _TransactionsScreenState extends State<TransactionsScreen> {
+  PageController _pageController = PageController(
+    initialPage: pages.singleTransactions.index,
+  );
+  String title = "Transactions";
+  int _currentPage = pages.singleTransactions.index;
+
   @override
   void initState() {
     DatabaseHelper.instance.pushGetAllTransactionsStream();
+    DatabaseHelper.instance.pushGetAlRecurringTransactionsStream();
     DatabaseHelper.instance.allAccountsStream.listen((event) {
       _accountList = event;
     });
@@ -32,6 +42,12 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
       _currentFilterAccountName = widget.initalAccountFilterName!;
     }
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   var _currentFilterAccountName = "";
@@ -79,39 +95,85 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
             },
           ),
         ],
-        title: const Text(
-          "Transactions",
-        ),
+        title: Text(title),
       ),
       drawer: createDrawer(context),
-      body: StreamBuilder<List<SingleTransaction>>(
-        stream: DatabaseHelper.instance.allTransactionStream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            var filteredList = snapshot.data!.where((element) {
-              if (_currentFilterAccountName == "") {
-                return true;
-              } else {
-                return element.account.name == _currentFilterAccountName ||
-                    (element.account2 != null &&
-                        element.account2!.name == _currentFilterAccountName);
-              }
-            }).toList();
-            return ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: filteredList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return TransactionItem(
-                  transactionData: filteredList[index],
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return const Text("Oops!");
-          }
-          return const Center(child: CircularProgressIndicator());
+      body: PageView(
+        onPageChanged: (int page) {
+          setState(() {
+            if (page == pages.singleTransactions.index) {
+              // title = "Budgets";
+              _currentPage = pages.singleTransactions.index;
+              DatabaseHelper.instance.pushGetAllTransactionsStream();
+            } else if (page == pages.recurringTransactions.index) {
+              // title = "Savings";
+              _currentPage = pages.recurringTransactions.index;
+              DatabaseHelper.instance.pushGetAlRecurringTransactionsStream();
+            }
+          });
         },
+        children: [
+          StreamBuilder<List<SingleTransaction>>(
+            stream: DatabaseHelper.instance.allTransactionStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var filteredList = snapshot.data!.where((element) {
+                  if (_currentFilterAccountName == "") {
+                    return true;
+                  } else {
+                    return element.account.name == _currentFilterAccountName ||
+                        (element.account2 != null &&
+                            element.account2!.name ==
+                                _currentFilterAccountName);
+                  }
+                }).toList();
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: filteredList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return TransactionItem(
+                      singleTransactionData: filteredList[index],
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return const Text("Oops!");
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+          StreamBuilder<List<RecurringTransaction>>(
+            stream: DatabaseHelper.instance.allRecurringTransactionStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var filteredList = snapshot.data!.where((element) {
+                  if (_currentFilterAccountName == "") {
+                    return true;
+                  } else {
+                    return element.account.name == _currentFilterAccountName ||
+                        (element.account2 != null &&
+                            element.account2!.name ==
+                                _currentFilterAccountName);
+                  }
+                }).toList();
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: filteredList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return TransactionItem(
+                      recurringTransactionData: filteredList[index],
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return const Text("Oops!");
+              }
+              return const Center(child: CircularProgressIndicator());
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -124,3 +186,127 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:budgetiser/db/database.dart';
+// import 'package:budgetiser/drawer.dart';
+// import 'package:budgetiser/screens/plans/budgetForm.dart';
+// import 'package:budgetiser/screens/plans/savingForm.dart';
+// import 'package:budgetiser/shared/dataClasses/budget.dart';
+// import 'package:budgetiser/shared/dataClasses/savings.dart';
+// import 'package:budgetiser/shared/widgets/budgetItem.dart';
+// import 'package:budgetiser/shared/widgets/savingItem.dart';
+// import 'package:flutter/material.dart';
+
+
+// c
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(title),
+//       ),
+//       drawer: createDrawer(context),
+//       body: PageView(
+//         onPageChanged: (int page) {
+//           setState(() {
+//             if (page == pages.budgets.index) {
+//               title = "Budgets";
+//               buttonTooltip = "Create Budget";
+//               _currentPage = pages.budgets.index;
+//               DatabaseHelper.instance.pushGetAllBudgetsStream();
+//             } else if (page == pages.savings.index) {
+//               title = "Savings";
+//               buttonTooltip = "Create Saving";
+//               _currentPage = pages.savings.index;
+//               DatabaseHelper.instance.pushGetAllSavingsStream();
+//             }
+//           });
+//         },
+//         children: [
+//           StreamBuilder<List<Budget>>(
+//             stream: DatabaseHelper.instance.allBudgetsStream,
+//             builder: (context, snapshot) {
+//               if(snapshot.hasData){
+//                 List<Budget> _budgets = snapshot.data!.toList();
+//                 _budgets.sort((a, b) => a.name.compareTo(b.name));
+//                 return ListView.builder(
+//                   scrollDirection: Axis.vertical,
+//                   itemCount: _budgets.length,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return BudgetItem(
+//                       budgetData: _budgets[index],
+//                     );
+//                   },
+//                 );
+//               }else if (snapshot.hasError) {
+//                 return const Text("Oops!");
+//               }
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             },
+//           ),
+//           StreamBuilder<List<Savings>>(
+//             stream: DatabaseHelper.instance.allSavingsStream,
+//             builder: (context, snapshot) {
+//               if(snapshot.hasData){
+//                 List<Savings> _savings = snapshot.data!.toList();
+//                 _savings.sort((a, b) => a.name.compareTo(b.name));
+//                 return ListView.builder(
+//                   scrollDirection: Axis.vertical,
+//                   itemCount: _savings.length,
+//                   itemBuilder: (BuildContext context, int index) {
+//                     return SavingItem(
+//                       savingData: _savings[index],
+//                     );
+//                   },
+//                 );
+//               }else if (snapshot.hasError) {
+//                 return const Text("Oops!");
+//               }
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             },
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: _currentPage == pages.budgets.index
+//           ? FloatingActionButton(
+//               tooltip: buttonTooltip,
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                     MaterialPageRoute(builder: (context) => BudgetForm()));
+//               },
+//               child: const Icon(Icons.add),
+//             )
+//           : FloatingActionButton(
+//               tooltip: buttonTooltip,
+//               onPressed: () {
+//                 Navigator.of(context).push(
+//                     MaterialPageRoute(builder: (context) => SavingForm()));
+//               },
+//               child: const Icon(Icons.add),
+//             ),
+//     );
+//   }
+// }
