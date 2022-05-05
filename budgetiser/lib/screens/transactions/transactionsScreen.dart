5bfd0eb530e1bc631ebb3b736215232c92451e5c
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-05-05 21:26:39 +0200

    icons to filter select

--- budgetiser/lib/screens/transactions/transactionsScreen.dart
+++ budgetiser/lib/screens/transactions/transactionsScreen.dart
@@ -147,8 +147,19 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
                       ),
                       for (var account in _accountList)
                         ListTile(
+                          title: Row(
+                            children: [
+                              Icon(
+                                account.icon,
+                                color: account.color,
+                              ),
+                              Flexible(
+                                child: Text(" " + account.name,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                            ],
+                          ),
                           visualDensity: VisualDensity.compact,
-                          title: Text(account.name),
                           leading: Radio(
                             value: account.name,
                             groupValue: _currentFilterAccountName,
@@ -160,7 +171,6 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
                             },
                           ),
                         ),
-                      // divider
                       const Divider(
                         indent: 25,
                       ),
@@ -187,8 +197,19 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
                       ),
                       for (var category in _categoryList)
                         ListTile(
+                          title: Row(
+                            children: [
+                              Icon(
+                                category.icon,
+                                color: category.color,
+                              ),
+                              Flexible(
+                                child: Text(" " + category.name,
+                                    overflow: TextOverflow.ellipsis),
+                              ),
+                            ],
+                          ),
                           visualDensity: VisualDensity.compact,
-                          title: Text(category.name),
                           leading: Radio(
                             value: category.name,
                             groupValue: _currentFilterCategoryName,
@@ -282,127 +303,3 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import 'package:budgetiser/db/database.dart';
-// import 'package:budgetiser/drawer.dart';
-// import 'package:budgetiser/screens/plans/budgetForm.dart';
-// import 'package:budgetiser/screens/plans/savingForm.dart';
-// import 'package:budgetiser/shared/dataClasses/budget.dart';
-// import 'package:budgetiser/shared/dataClasses/savings.dart';
-// import 'package:budgetiser/shared/widgets/budgetItem.dart';
-// import 'package:budgetiser/shared/widgets/savingItem.dart';
-// import 'package:flutter/material.dart';
-
-
-// c
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       drawer: createDrawer(context),
-//       body: PageView(
-//         onPageChanged: (int page) {
-//           setState(() {
-//             if (page == pages.budgets.index) {
-//               title = "Budgets";
-//               buttonTooltip = "Create Budget";
-//               _currentPage = pages.budgets.index;
-//               DatabaseHelper.instance.pushGetAllBudgetsStream();
-//             } else if (page == pages.savings.index) {
-//               title = "Savings";
-//               buttonTooltip = "Create Saving";
-//               _currentPage = pages.savings.index;
-//               DatabaseHelper.instance.pushGetAllSavingsStream();
-//             }
-//           });
-//         },
-//         children: [
-//           StreamBuilder<List<Budget>>(
-//             stream: DatabaseHelper.instance.allBudgetsStream,
-//             builder: (context, snapshot) {
-//               if(snapshot.hasData){
-//                 List<Budget> _budgets = snapshot.data!.toList();
-//                 _budgets.sort((a, b) => a.name.compareTo(b.name));
-//                 return ListView.builder(
-//                   scrollDirection: Axis.vertical,
-//                   itemCount: _budgets.length,
-//                   itemBuilder: (BuildContext context, int index) {
-//                     return BudgetItem(
-//                       budgetData: _budgets[index],
-//                     );
-//                   },
-//                 );
-//               }else if (snapshot.hasError) {
-//                 return const Text("Oops!");
-//               }
-//               return const Center(
-//                 child: CircularProgressIndicator(),
-//               );
-//             },
-//           ),
-//           StreamBuilder<List<Savings>>(
-//             stream: DatabaseHelper.instance.allSavingsStream,
-//             builder: (context, snapshot) {
-//               if(snapshot.hasData){
-//                 List<Savings> _savings = snapshot.data!.toList();
-//                 _savings.sort((a, b) => a.name.compareTo(b.name));
-//                 return ListView.builder(
-//                   scrollDirection: Axis.vertical,
-//                   itemCount: _savings.length,
-//                   itemBuilder: (BuildContext context, int index) {
-//                     return SavingItem(
-//                       savingData: _savings[index],
-//                     );
-//                   },
-//                 );
-//               }else if (snapshot.hasError) {
-//                 return const Text("Oops!");
-//               }
-//               return const Center(
-//                 child: CircularProgressIndicator(),
-//               );
-//             },
-//           ),
-//         ],
-//       ),
-//       floatingActionButton: _currentPage == pages.budgets.index
-//           ? FloatingActionButton(
-//               tooltip: buttonTooltip,
-//               onPressed: () {
-//                 Navigator.of(context).push(
-//                     MaterialPageRoute(builder: (context) => BudgetForm()));
-//               },
-//               child: const Icon(Icons.add),
-//             )
-//           : FloatingActionButton(
-//               tooltip: buttonTooltip,
-//               onPressed: () {
-//                 Navigator.of(context).push(
-//                     MaterialPageRoute(builder: (context) => SavingForm()));
-//               },
-//               child: const Icon(Icons.add),
-//             ),
-//     );
-//   }
-// }
