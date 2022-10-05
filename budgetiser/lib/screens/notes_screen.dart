f91623f805041c868aef814060932ba6f1221d37
Edmund <75438446+minerop5000@users.noreply.github.com>
2022-10-05 14:16:13 +0200

    save notes in device preferences

--- budgetiser/lib/screens/notes_screen.dart
+++ budgetiser/lib/screens/notes_screen.dart
@@ -1,5 +1,6 @@
 import 'package:budgetiser/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class NotesScreen extends StatelessWidget {
   static String routeID = 'notes123';
@@ -9,11 +10,39 @@ class NotesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    onInit();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Notes",
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Notes'),
+                    content: const Text(
+                        'This is a place to store notes. The notes are stored on the device and cannot be im- or exported.'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Close'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                    elevation: 0,
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
       drawer: createDrawer(context),
       body: Padding(
@@ -24,8 +53,20 @@ class NotesScreen extends StatelessWidget {
           decoration: const InputDecoration(
             border: OutlineInputBorder(),
           ),
+          autofocus: true,
+          onChanged: onChanged,
         ),
       ),
     );
+  }
+
+  onInit() async {
+    final prefs = await SharedPreferences.getInstance();
+    valueController.text = prefs.getString('key-notes') ?? '';
+  }
+
+  onChanged(String string) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('key-notes', string);
   }
 }
