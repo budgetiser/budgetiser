b75084ace80dcaba5afcbc1334d5909c64f43da6
aschulte201 <39188057+aschulte201@users.noreply.github.com>
2022-10-12 17:47:59 +0200

    added new color DARK_CLICKABLE (#106)

--- budgetiser/lib/config/themes/themes.dart
+++ budgetiser/lib/config/themes/themes.dart
@@ -32,7 +32,9 @@ class MyThemes {
     toggleableActiveColor: LIGHT_PRIMARY,
   );
 
+
   static const Color DARK_PRIMARY = Color.fromARGB(255, 50, 130, 184);
+  static const Color DARK_CLICKABLE = Color.fromARGB(255, 50, 130, 184);
   static const Color DARK_SECONDARY = Color.fromARGB(255, 15, 76, 117);
   static const Color DARK_TERTIARY = Color.fromARGB(255, 187, 225, 250);
   static const Color DARK_BACKGROUND = Color.fromARGB(255, 27, 38, 44);
@@ -44,9 +46,29 @@ class MyThemes {
       secondary: DARK_SECONDARY,
       tertiary: DARK_TERTIARY,
     ),
+
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(
+          color: DARK_CLICKABLE
+      ),
+      color: DARK_BACKGROUND,
+      elevation: 0,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        // title text in appbar
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+
+    drawerTheme: DrawerThemeData(
+      backgroundColor: DARK_BACKGROUND,
+    ),
+
     inputDecorationTheme: const InputDecorationTheme(
       errorStyle: TextStyle(fontSize: 18),
     ),
+
     textTheme: const TextTheme(
       caption: TextStyle(
           color: Color.fromARGB(255, 255, 255, 255),
@@ -62,22 +84,15 @@ class MyThemes {
       subtitle1: TextStyle(fontSize: 20), // for the drawer items
       bodyText1: TextStyle(fontSize: 20, color: Colors.grey), // ??
     ),
-    primaryColor: Colors.black,
+
     dividerTheme: const DividerThemeData(
       thickness: 1.5,
       color: Color.fromARGB(50, 255, 255, 255),
     ),
-    toggleableActiveColor: DARK_PRIMARY,
-    appBarTheme: const AppBarTheme(
-      color: DARK_BACKGROUND,
-      elevation: 0,
-      centerTitle: true,
-      titleTextStyle: TextStyle(
-        // title text in appbar
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
+
+    //Checkboxes, Radios, ...
+    toggleableActiveColor: DARK_CLICKABLE,
+
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       backgroundColor: DARK_PRIMARY,
     ),
