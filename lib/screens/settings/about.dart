cf7eeaa17c3f374140d7f8f24b8a1416fc3eff37
Eddi <75438446+minerop5000@users.noreply.github.com>
2023-07-10 19:45:59 +0200

    init about tile

---
+++ lib/screens/settings/about.dart
@@ -0,0 +1,31 @@
+import 'package:budgetiser/drawer.dart';
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "About",
+        ),
+      ),
+      drawer: createDrawer(context),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: const Text('GitHub'),
+              subtitle: const Text(
+                'TODO',
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
