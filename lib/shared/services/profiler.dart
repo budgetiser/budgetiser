9631812ae5d2c50308cdd5f86f5bbbad0bdd37d2
aschulte201 <a.schulte201@gmail.com>
2023-10-21 16:45:19 +0200

    formatting

--- lib/shared/services/profiler.dart
+++ lib/shared/services/profiler.dart
@@ -1,4 +1,4 @@
-import 'dart:developer';
+import 'package:flutter/foundation.dart';
 
 class Profiler {
   Profiler._privateConstructor();
@@ -6,21 +6,21 @@ class Profiler {
   static final Profiler instance = Profiler._privateConstructor();
 
   Map<String, Map<String, dynamic>> timeMeasurements = {};
-  String lastStarted = "";
+  String lastStarted = '';
 
   void start(String eventName) {
     bool add = false;
     if (!timeMeasurements.containsKey(eventName)) {
       timeMeasurements[eventName] = {
-        "name": eventName,
-        "start": <int>[],
-        "end": <int>[],
-        "count": 0,
+        'name': eventName,
+        'start': <int>[],
+        'end': <int>[],
+        'count': 0,
       };
       add = true;
-    } else if (timeMeasurements[eventName]!['start'].length >
-        timeMeasurements[eventName]!['end'].length) {
-      print("Time measure error: Event '$eventName' not finished before reassignment!");
+    } else if ((timeMeasurements[eventName]!['start'] as List<int>).length >
+        (timeMeasurements[eventName]!['end'] as List<int>).length) {
+      throw 'Time measure error: Event "$eventName" not finished before reassignment!';
     } else {
       add = true;
     }
@@ -28,7 +28,8 @@ class Profiler {
     // Start measurement as late as possible
     if (add) {
       lastStarted = eventName;
-      timeMeasurements[eventName]!['start'].add(DateTime.now().microsecondsSinceEpoch);
+      (timeMeasurements[eventName]!['start'] as List<int>)
+          .add(DateTime.now().microsecondsSinceEpoch);
     }
   }
 
@@ -37,13 +38,14 @@ class Profiler {
     // End measurement as fast as possible
     final tempTime = DateTime.now().microsecondsSinceEpoch;
     if (!timeMeasurements.containsKey(name)) {
-      print("Time measure error: Event '$name' not defined!");
-    } else if (timeMeasurements[name]!['end'].length >=
-        timeMeasurements[name]!['start'].length) {
-      print("Time measure error: Event '$name' not started before reassignment!");
+      throw 'Time measure error: Event "$name" not defined!';
+    } else if ((timeMeasurements[name]!['end'] as List<int>).length >=
+        (timeMeasurements[name]!['start'] as List<int>).length) {
+      throw 'Time measure error: Event "$name" not started before reassignment!';
     } else {
-      timeMeasurements[name]!['end'].add(tempTime);
-      timeMeasurements[name]!['count']++;
+      (timeMeasurements[name]!['end'] as List<int>).add(tempTime);
+      timeMeasurements[name]!['count'] =
+          (timeMeasurements[name]!['count'] as int) + 1;
     }
     analyseTimeMeasurements();
   }
@@ -51,16 +53,16 @@ class Profiler {
   void analyseTimeMeasurements() {
     // Analyze time measurements and print them in a table.
     // Multiple events with the same name are averaged.
-
-    print('Name\tAvg. [ms]\tMin. [ms]\tMax. [ms]\tOccurrences [compl.]');
+    if (kDebugMode) {
+      print('Name\tAvg. [ms]\tMin. [ms]\tMax. [ms]\tOccurrences [compl.]');
+    }
     timeMeasurements.forEach((key, event) {
       final List<int> startTimes = event['start'];
       final List<int> endTimes = event['end'];
       final List<int> timings = [];
 
       if (startTimes.length != endTimes.length) {
-        print("Time measure error: Event '$key' has different amounts of values for start and end times!");
-        return;
+        throw 'Time measure error: Event "$key" has different amounts of values for start and end times!';
       }
 
       for (int i = 0; i < startTimes.length; i++) {
@@ -72,11 +74,18 @@ class Profiler {
         }
       }
 
-      final minTiming = timings.isEmpty ? 0 : timings.reduce((a, b) => a < b ? a : b);
-      final maxTiming = timings.isEmpty ? 0 : timings.reduce((a, b) => a > b ? a : b);
-      final avgTiming = timings.isEmpty ? 0 : timings.reduce((a, b) => a + b) / timings.length;
+      final minTiming =
+          timings.isEmpty ? 0 : timings.reduce((a, b) => a < b ? a : b);
+      final maxTiming =
+          timings.isEmpty ? 0 : timings.reduce((a, b) => a > b ? a : b);
+      final avgTiming = timings.isEmpty
+          ? 0
+          : timings.reduce((a, b) => a + b) / timings.length;
 
-      print('$key\t${avgTiming.toStringAsFixed(2)}\t${minTiming.toStringAsFixed(2)}\t${maxTiming.toStringAsFixed(2)}\t${event['count']}');
+      if (kDebugMode) {
+        print(
+            '$key\t${avgTiming.toStringAsFixed(2)}\t${minTiming.toStringAsFixed(2)}\t${maxTiming.toStringAsFixed(2)}\t${event['count']}');
+      }
     });
   }
 }
