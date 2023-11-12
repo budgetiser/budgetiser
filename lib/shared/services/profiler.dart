92396d4cbd7812b9840bbc2e6921ecd9cef7ba0a
Eddi <75438446+minerop5000@users.noreply.github.com>
2023-11-12 16:47:20 +0100

    add transaction profiler events

--- lib/shared/services/profiler.dart
+++ lib/shared/services/profiler.dart
@@ -1,15 +1,11 @@
 import 'dart:developer';
 
-import 'package:uuid/uuid.dart';
-
 class Profiler {
   Profiler._privateConstructor();
 
   static final Profiler instance = Profiler._privateConstructor();
 
   final _timeline = TimelineTask(filterKey: 'profiler-UID-993e63f6');
-
-  var uuid = const Uuid();
 
   void start(String eventName) {
     _timeline.start(eventName);
