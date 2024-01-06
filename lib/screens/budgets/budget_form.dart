74c86d817727bc9b09af8e57f7193c25530d924b
Eddi <75438446+minerop5000@users.noreply.github.com>
2024-01-06 15:14:42 +0100

    fix budget description form

--- lib/screens/budgets/budget_form.dart
+++ lib/screens/budgets/budget_form.dart
@@ -36,6 +36,7 @@ class _BudgetFormState extends State<BudgetForm> {
     if (widget.budgetData != null) {
       nameController.text = widget.budgetData!.name;
       limitController.text = widget.budgetData!.maxValue.toString();
+      descriptionController.text = widget.budgetData!.description ?? '';
       _color = widget.budgetData!.color;
       _icon = widget.budgetData!.icon;
       budgetCategories = widget.budgetData!.transactionCategories;
