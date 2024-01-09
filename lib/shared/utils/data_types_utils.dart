aa6ce3c5141c200096bc8dea3e6476148c41881a
aschulte201 <a.schulte201@gmail.com>
2024-01-09 19:20:59 +0100

    refactor: removed unused method

--- lib/shared/utils/data_types_utils.dart
+++ lib/shared/utils/data_types_utils.dart
@@ -4,21 +4,3 @@ import 'package:budgetiser/core/database/models/budget.dart';
 double roundDouble(double value) {
   return double.parse(value.toStringAsFixed(2));
 }
-
-/// get interval representation as string
-String getIntervalString(IntervalUnit unit) {
-  switch (unit) {
-    case IntervalUnit.day:
-      return 'daily';
-    case IntervalUnit.week:
-      return 'weekly';
-    case IntervalUnit.month:
-      return 'monthly';
-    case IntervalUnit.quarter:
-      return 'quarterly';
-    case IntervalUnit.year:
-      return 'yearly';
-    default:
-      return '';
-  }
-}
